2733cfb2d12e41622456e1ef8e5b1a5d0bdfdc64
syahrulhidayat <133508053+syahrulhidayat@users.noreply.github.com>
2026-06-22 13:21:01 +0000

    Update arsitektur mshqc, sinkronisasi file terhapus, dan optimasi struktur

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """
 Setup script for MSHQC Python bindings
-File: /home/syahrul/mshqc/setup.py
+File: /workspaces/mshqc/setup.py
 """
 
 from setuptools import setup, Extension
@@ -25,7 +25,13 @@ class CMakeBuild(build_ext):
             self.build_extension(ext)
 
     def build_extension(self, ext):
+        # extdir adalah folder staging tempat file .whl dirakit
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        
+        # Pastikan output dari cmake masuk tepat ke dalam folder 'mshqc'
+        if not extdir.endswith("mshqc"):
+            extdir = os.path.join(extdir, "mshqc")
+        os.makedirs(extdir, exist_ok=True)
         
         import nanobind
         nanobind_cmake_path = nanobind.cmake_dir()
@@ -33,17 +39,15 @@ class CMakeBuild(build_ext):
         # CMake config
         cmake_args = [
             f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
+            f'-DCMAKE_RUNTIME_OUTPUT_DIRECTORY={extdir}', # Menangkap libmshqc.so
             f'-DPYTHON_EXECUTABLE={sys.executable}',
-            
-            # [UBAH BARIS INI]
             f'-Dnanobind_DIR={nanobind_cmake_path}',  
-            
             '-DCMAKE_BUILD_TYPE=Release',
         ]
         
         build_args = ['--config', 'Release']
         
-        # Set threads
+        # Set eksekusi paralel OpenMP/Kompilasi
         if 'CMAKE_BUILD_PARALLEL_LEVEL' not in os.environ:
             build_args += ['-j4']
         
@@ -55,32 +59,40 @@ class CMakeBuild(build_ext):
             env.get('CXXFLAGS', ''),
             self.distribution.get_version())
 
-        # Configure
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, 
-                            cwd=self.build_temp, env=env)
-        # Build
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, 
-                            cwd=self.build_temp)
+        # Configure & Build
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
-# Read long description
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# Read long description safely
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "MSHQC Quantum Mechanics Library"
 
 setup(
     name="mshqc",
     version="1.0.0",
-    author="MSHQC Team",
+    author="Muhamad Syahrul Hidayat",
     description="Multi-State High-Quality Calculations",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=['mshqc'],
     package_dir={'': 'python'},
+    
+    # Memastikan file biner terbungkus dengan aman (zip_safe=False sangat penting untuk C++)
+    package_data={
+        "mshqc": ["*.so", "*.pyi"],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    
     ext_modules=[CMakeExtension('mshqc._mshqc')],
     cmdclass=dict(build_ext=CMakeBuild),
     install_requires=[
         'numpy>=1.20.0',
         'scipy>=1.7.0',
-        'nanobind>=1.9.0', # Wajib ada di sini
+        'nanobind>=1.9.0',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
