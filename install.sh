cc59acfe525d2a778c2bcb3b0e439ff452bbdb10
Muhamad Syahrul Hidayat <133508053+syahrulhidayat@users.noreply.github.com>
2026-06-25 15:56:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-git_pull origin main
-/opt/conda/bin/python -m pip install releases/*.whl --force-reinstall
+#!/bin/bash
+
+# Tarik pembaruan terbaru dari GitHub
+git pull origin main
+
+# Gunakan 'python' yang aktif saat ini, jangan gunakan jalur absolut absolut /opt/conda/...
+python -m pip install releases/*.whl --force-reinstall
