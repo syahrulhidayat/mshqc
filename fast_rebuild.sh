6d1868f9a5178e5902559a0054ebb73ff413a4c4
Syahrul Hidayat <hidayatsyahrul53@gmail.com>
2026-06-26 14:40:14 +0700

    Update arsitektur mshqc, sinkronisasi file terhapus, dan optimasi struktur

--- fast_rebuild.sh
+++ fast_rebuild.sh
@@ -2,16 +2,16 @@
 set -e
 
 echo -e "\033[1;36m=================================================================\033[0m"
-echo -e "\033[1;36m           MSHQC LOKAL TURBO BUILD (DEVELOPER MODE)              \033[0m"
+echo -e "\033[1;36m              MEMBUAT FILE .WHL LOKAL (DENGAN CCACHE)            \033[0m"
 echo -e "\033[1;36m=================================================================\033[0m"
 
-# Pastikan ccache digunakan sebagai compiler utama
+# Pastikan menggunakan ccache agar prosesnya instan
 export CC="ccache $CONDA_PREFIX/bin/x86_64-conda-linux-gnu-cc"
 export CXX="ccache $CONDA_PREFIX/bin/x86_64-conda-linux-gnu-c++"
 export CXXFLAGS="-std=c++17 -fopenmp -O3 -mavx2 -mfma -mtune=native -DEIGEN_USE_BLAS -DEIGEN_USE_LAPACKE -DNDEBUG"
 
-echo -e "\033[1;33m[*] Mengompilasi C++ (Hanya file yang berubah)...\033[0m"
-# build_ext --inplace adalah kunci agar kompilasi langsung jadi file .so di tempat, tanpa membuat .whl
-python setup.py build_ext --inplace
+echo -e "\033[1;33m[*] Merakit Python Wheel (.whl)...\033[0m"
+# Perintah ini yang membungkus C++ dan Python menjadi file .whl
+python setup.py bdist_wheel
 
-echo -e "\033[1;32m[*] MSHQC Lokal Siap! Ccache menyimpan waktu Anda.\033[0m"
+echo -e "\033[1;32m[*] Selesai! File .whl Anda ada di dalam folder dist/\033[0m"
