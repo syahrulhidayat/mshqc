4e2fb6d1361aab3d5dbac20819be7399959f1910
MSHQC Automation <hidayatsyahrul53@gmail.com>
2026-07-17 13:18:12 +0700

    fix beta

--- src/mp/omp_fock.cc
+++ src/mp/omp_fock.cc
@@ -586,7 +586,7 @@ void OMP2::build_generalized_fock() {
                             if (is_restricted) {
                                 L_ijab = 2.0 * (*t_aa_blk)(i, a, j, b) - 1.0 * (*t_aa_blk)(i, b, j, a);
                             } else {
-                                L_ijab = 0.5 * (*t_aa_blk)(i, a, j, b);
+                                L_ijab = (*t_aa_blk)(i, a, j, b); 
                             } 
                             T2_aa(i*va_+a, j*va_+b) = L_ijab; 
                         }
@@ -596,37 +596,9 @@ void OMP2::build_generalized_fock() {
         }
 
         Eigen::MatrixXd X_a = T2_aa * B_ia_P_alpha_;
-        
-        Eigen::MatrixXd B_oo_a = Eigen::MatrixXd::Zero(na_*na_, n_aux);
-        Eigen::MatrixXd B_vv_a = Eigen::MatrixXd::Zero(va_*va_, n_aux);
+        Eigen::MatrixXd X_b; 
 
-        const Eigen::MatrixXd& Ca_o = scf_.C_alpha.leftCols(na_);
-        const Eigen::MatrixXd& Ca_v = scf_.C_alpha.rightCols(va_);
-
-        for (int P = 0; P < n_aux; ++P) {
-            Eigen::Map<const Eigen::MatrixXd> B_AO(scf_.L_mat.col(P).data(), nbf_, nbf_);
-            Eigen::MatrixXd MO_oo_a = Ca_o.transpose() * (B_AO * Ca_o);
-            Eigen::MatrixXd MO_vv_a = Ca_v.transpose() * (B_AO * Ca_v);
-
-            for(int i=0; i<na_; ++i) for(int j=0; j<na_; ++j) B_oo_a(i*na_+j, P) = MO_oo_a(i, j);
-            for(int a=0; a<va_; ++a) for(int b=0; b<va_; ++b) B_vv_a(a*va_+b, P) = MO_vv_a(a, b);
-        }
-
-        Z_mat_a.setZero();
-
-        for (int P = 0; P < n_aux; ++P) {
-        
-            Eigen::Map<const Eigen::MatrixXd> XT_a(X_a.col(P).data(), va_, na_); 
-            Eigen::Map<const Eigen::MatrixXd> V_a(B_vv_a.col(P).data(), va_, va_);
-            Eigen::Map<const Eigen::MatrixXd> O_a(B_oo_a.col(P).data(), na_, na_);
-            
-            
-            Z_mat_a.noalias() += V_a * XT_a - XT_a * O_a;
-        }
-
-        if (is_restricted) {
-            Z_mat_b = Z_mat_a;
-        } else if (nb_ > 0 && vb_ > 0) {
+        if (!is_restricted && nb_ > 0 && vb_ > 0) {
             Eigen::MatrixXd T2_ab = Eigen::MatrixXd::Zero(na_*va_, nb_*vb_);
             Eigen::MatrixXd T2_bb = Eigen::MatrixXd::Zero(nb_*vb_, nb_*vb_);
             
@@ -637,7 +609,7 @@ void OMP2::build_generalized_fock() {
                     for (int j = 0; j < nb_; ++j) {
                         for (int a = 0; a < va_; ++a) {
                             for (int b = 0; b < vb_; ++b) {
-                                T2_ab(i*va_+a, j*vb_+b) = 0.5 * (*t_ab_blk)(i, j, a, b);
+                                T2_ab(i*va_+a, j*vb_+b) = (*t_ab_blk)(i, j, a, b);
                             }
                         }
                     }
@@ -650,7 +622,7 @@ void OMP2::build_generalized_fock() {
                     for (int j = 0; j < nb_; ++j) {
                         for (int a = 0; a < vb_; ++a) {
                             for (int b = 0; b < vb_; ++b) {
-                                T2_bb(i*vb_+a, j*vb_+b) = 0.5 * (*t_bb_blk)(i, j, a, b);
+                                T2_bb(i*vb_+a, j*vb_+b) = (*t_bb_blk)(i, j, a, b);
                             }
                         }
                     }
@@ -658,8 +630,32 @@ void OMP2::build_generalized_fock() {
             }
             
             X_a += T2_ab * B_ia_P_beta_;
-            Eigen::MatrixXd X_b = T2_bb * B_ia_P_beta_ + T2_ab.transpose() * B_ia_P_alpha_;
+            X_b = T2_bb * B_ia_P_beta_ + T2_ab.transpose() * B_ia_P_alpha_;
+        }
+        Eigen::MatrixXd B_oo_a = Eigen::MatrixXd::Zero(na_*na_, n_aux);
+        Eigen::MatrixXd B_vv_a = Eigen::MatrixXd::Zero(va_*va_, n_aux);
+        const Eigen::MatrixXd& Ca_o = scf_.C_alpha.leftCols(na_);
+        const Eigen::MatrixXd& Ca_v = scf_.C_alpha.rightCols(va_);
 
+        for (int P = 0; P < n_aux; ++P) {
+            Eigen::Map<const Eigen::MatrixXd> B_AO(scf_.L_mat.col(P).data(), nbf_, nbf_);
+            Eigen::MatrixXd MO_oo_a = Ca_o.transpose() * (B_AO * Ca_o);
+            Eigen::MatrixXd MO_vv_a = Ca_v.transpose() * (B_AO * Ca_v);
+
+            for(int i=0; i<na_; ++i) for(int j=0; j<na_; ++j) B_oo_a(i*na_+j, P) = MO_oo_a(i, j);
+            for(int a=0; a<va_; ++a) for(int b=0; b<va_; ++b) B_vv_a(a*va_+b, P) = MO_vv_a(a, b);
+        }
+        Z_mat_a.setZero();
+        for (int P = 0; P < n_aux; ++P) {
+            Eigen::Map<const Eigen::MatrixXd> XT_a(X_a.col(P).data(), va_, na_); 
+            Eigen::Map<const Eigen::MatrixXd> V_a(B_vv_a.col(P).data(), va_, va_);
+            Eigen::Map<const Eigen::MatrixXd> O_a(B_oo_a.col(P).data(), na_, na_);
+            Z_mat_a.noalias() += V_a * XT_a - XT_a * O_a;
+        }
+
+        if (is_restricted) {
+            Z_mat_b = Z_mat_a;
+        } else if (nb_ > 0 && vb_ > 0) {
             Eigen::MatrixXd B_oo_b = Eigen::MatrixXd::Zero(nb_*nb_, n_aux);
             Eigen::MatrixXd B_vv_b = Eigen::MatrixXd::Zero(vb_*vb_, n_aux);
             const Eigen::MatrixXd& Cb_o = scf_.C_beta.leftCols(nb_);
@@ -674,7 +670,6 @@ void OMP2::build_generalized_fock() {
             }
 
             Z_mat_b.setZero();
-           
             for (int P = 0; P < n_aux; ++P) {
                 Eigen::Map<const Eigen::MatrixXd> XT_b(X_b.col(P).data(), vb_, nb_); 
                 Eigen::Map<const Eigen::MatrixXd> V_b(B_vv_b.col(P).data(), vb_, vb_);
