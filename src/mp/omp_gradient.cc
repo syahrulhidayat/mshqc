2b75ce441fb146cf2494cb24870636b1f95c7f72
MSHQC Automation <hidayatsyahrul53@gmail.com>
2026-07-17 12:48:05 +0700

    fix koef

--- src/mp/omp_gradient.cc
+++ src/mp/omp_gradient.cc
@@ -105,8 +105,7 @@ void OMP2::evaluate_z_vector_cholesky(Eigen::MatrixXd& Z_mat_a, Eigen::MatrixXd&
                 if (is_restricted) {
                     X_a_chunk.noalias() += T2_rmp2 * Bia_chunk;
                 } else {
-                    // [MODIFIKASI] Kalikan 0.5 untuk koreksi perhitungan ganda spin-sejenis
-                    X_a_chunk.noalias() += T2_aa_mat * Bia_chunk; 
+                    X_a_chunk.noalias() += 2.0 * T2_aa_mat * Bia_chunk; 
                 }
             }
 
@@ -114,14 +113,12 @@ void OMP2::evaluate_z_vector_cholesky(Eigen::MatrixXd& Z_mat_a, Eigen::MatrixXd&
                 Eigen::MatrixXd Bib_chunk = B_ia_P_beta_.middleCols(P_start, P_len);
                 
                 if (t_ab_blk) {
-                    // Blok alpha-beta (beda-spin) TIDAK butuh faktor 0.5
-                    X_a_chunk.noalias() += T2_ab_mat * Bib_chunk;
-                    X_b_chunk.noalias() += T2_ab_mat.transpose() * Bia_chunk;
+                    X_a_chunk.noalias() += 2.0 * T2_ab_mat * Bib_chunk;
+                    X_b_chunk.noalias() += 2.0 * T2_ab_mat.transpose() * Bia_chunk;
                 }
 
                 if (t_bb_blk) {
-                    // [MODIFIKASI] Kalikan 0.5 untuk koreksi perhitungan ganda spin-sejenis
-                    X_b_chunk.noalias() +=  T2_bb_mat * Bib_chunk; 
+                    X_b_chunk.noalias() += 2.0 * T2_bb_mat * Bib_chunk; 
                 }
             }
 
