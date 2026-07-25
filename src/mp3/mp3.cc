9781e3898ffce84700e632962dc4be1881d45466
MSHQC Automation <hidayatsyahrul53@gmail.com>
2026-07-26 03:14:09 +0700

    fix

--- src/mp3/mp3.cc
+++ src/mp3/mp3.cc
@@ -1096,13 +1096,15 @@ void OMP3::build_generalized_fock() {
         for (int a = 0; a < va_; ++a) {
             for (int j = 0; j < na_; ++j) {
                 for (int b = 0; b < va_; ++b) {
-                    // Perbaikan: Tambahkan 2.0 * pada L2
-                    double t2_dir = T2_aa_ijab(i, j, a, b) +  L2_aa_(i, j, a, b);
+                    double t1_dir = T2_aa_ijab(i, j, a, b);
+                    double t2_dir = L2_aa_(i, j, a, b);
                     if (is_restricted) {
-                        double t2_ex = T2_aa_ijab(i, j, b, a) + L2_aa_(i, j, b, a); 
-                        Teff_aa(i*va_+a, j*va_+b) = (2.0 * t2_dir - 1.0 * t2_ex);
+                        double t1_ex = T2_aa_ijab(i, j, b, a);
+                        double t2_ex = L2_aa_(i, j, b, a); 
+                        // Teff = T1 + 2 * T2
+                        Teff_aa(i*va_+a, j*va_+b) = 2.0 * (t1_dir + 2.0 * t2_dir) - 1.0 * (t1_ex + 2.0 * t2_ex);
                     } else {
-                        Teff_aa(i*va_+a, j*va_+b) = 2.0 * t2_dir; 
+                        Teff_aa(i*va_+a, j*va_+b) = 2.0 * (t1_dir + 2.0 * t2_dir); 
                     }
                 }
             }
@@ -1182,8 +1184,9 @@ void OMP3::build_generalized_fock() {
             for (int a = 0; a < va_; ++a) {
                 for (int j = 0; j < nb_; ++j) {
                     for (int b = 0; b < vb_; ++b) {
-                        double t2_dir = (*t2_ab_dense)(i, j, a, b) + L2_ab_(i, j, a, b);
-                        Teff_ab(i*va_+a, j*vb_+b) = 2.0 * t2_dir; 
+                        double t1_dir = (*t2_ab_dense)(i, j, a, b);
+                        double t2_dir = L2_ab_(i, j, a, b);
+                        Teff_ab(i*va_+a, j*vb_+b) = 2.0 * (t1_dir + 2.0 * t2_dir); 
                     }
                 }
             }
@@ -1194,8 +1197,9 @@ void OMP3::build_generalized_fock() {
             for (int a = 0; a < vb_; ++a) {
                 for (int j = 0; j < nb_; ++j) {
                     for (int b = 0; b < vb_; ++b) {
-                        double t2_dir = (*t2_bb_dense)(i, j, a, b) + L2_bb_(i, j, a, b);
-                        Teff_bb(i*vb_+a, j*vb_+b) = 2.0 * t2_dir; 
+                        double t1_dir = (*t2_bb_dense)(i, j, a, b);
+                        double t2_dir = L2_bb_(i, j, a, b);
+                        Teff_bb(i*vb_+a, j*vb_+b) = 2.0 * (t1_dir + 2.0 * t2_dir); 
                     }
                 }
             }
