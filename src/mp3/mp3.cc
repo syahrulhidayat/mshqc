93ad8a4d36d54a410b88cc91cd7995006f87d111
MSHQC Automation <hidayatsyahrul53@gmail.com>
2026-07-26 02:09:21 +0700

    fix

--- src/mp3/mp3.cc
+++ src/mp3/mp3.cc
@@ -1108,9 +1108,9 @@ void OMP3::build_generalized_fock() {
             for (int j = 0; j < na_; ++j) {
                 for (int b = 0; b < va_; ++b) {
                     // Perbaikan: Tambahkan 2.0 * pada L2
-                    double t2_dir = T2_aa_ijab(i, j, a, b) + 2.0 * L2_aa_(i, j, a, b);
+                    double t2_dir = T2_aa_ijab(i, j, a, b) +  L2_aa_(i, j, a, b);
                     if (is_restricted) {
-                        double t2_ex = T2_aa_ijab(i, j, b, a) + 2.0 * L2_aa_(i, j, b, a); 
+                        double t2_ex = T2_aa_ijab(i, j, b, a) + L2_aa_(i, j, b, a); 
                         Teff_aa(i*va_+a, j*va_+b) = (2.0 * t2_dir - 1.0 * t2_ex);
                     } else {
                         Teff_aa(i*va_+a, j*va_+b) = 2.0 * t2_dir; 
@@ -1193,7 +1193,7 @@ void OMP3::build_generalized_fock() {
             for (int a = 0; a < va_; ++a) {
                 for (int j = 0; j < nb_; ++j) {
                     for (int b = 0; b < vb_; ++b) {
-                        double t2_dir = (*t2_ab_dense)(i, j, a, b) + 2.0 * L2_ab_(i, j, a, b);
+                        double t2_dir = (*t2_ab_dense)(i, j, a, b) + L2_ab_(i, j, a, b);
                         Teff_ab(i*va_+a, j*vb_+b) = 2.0 * t2_dir; 
                     }
                 }
@@ -1205,7 +1205,7 @@ void OMP3::build_generalized_fock() {
             for (int a = 0; a < vb_; ++a) {
                 for (int j = 0; j < nb_; ++j) {
                     for (int b = 0; b < vb_; ++b) {
-                        double t2_dir = (*t2_bb_dense)(i, j, a, b) + 2.0 * L2_bb_(i, j, a, b);
+                        double t2_dir = (*t2_bb_dense)(i, j, a, b) + L2_bb_(i, j, a, b);
                         Teff_bb(i*vb_+a, j*vb_+b) = 2.0 * t2_dir; 
                     }
                 }
