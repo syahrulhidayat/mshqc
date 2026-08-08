80207a294488b983dcac5e6e4e3eb684894329ff
MSHQC Automation <automation@mshqc.internal>
2026-08-08 14:42:41 +0700

    fix(compiler): implement custom vectorization pattern wrapping mlir::linalg::vectorize for llvm 22 compatibility

--- src/compiler/Passes/TilingPass.cc
+++ src/compiler/Passes/TilingPass.cc
@@ -13,6 +13,20 @@ namespace compiler {
 #include "Passes.h.inc"
 
 namespace {
+
+namespace {
+struct GenericVectorizationPattern : public mlir::OpInterfaceRewritePattern<mlir::linalg::LinalgOp> {
+    using OpInterfaceRewritePattern<mlir::linalg::LinalgOp>::OpInterfaceRewritePattern;
+    mlir::LogicalResult matchAndRewrite(mlir::linalg::LinalgOp op, mlir::PatternRewriter &rewriter) const override {
+        // Pemanggilan utilitas MLIR Vectorize modern
+        if (mlir::failed(mlir::linalg::vectorize(rewriter, op))) {
+            return mlir::failure();
+        }
+        return mlir::success();
+    }
+};
+} // namespace
+
 struct LinalgTilingPass : public impl::LinalgTilingBase<LinalgTilingPass> {
     using LinalgTilingBase::LinalgTilingBase;
 
@@ -55,7 +69,7 @@ struct LinalgTilingPass : public impl::LinalgTilingBase<LinalgTilingPass> {
 
         // Pemaksaan vektorisasi Linalg ke representasi intrinsik Vector Dialect
         mlir::RewritePatternSet vectorPatterns(&getContext());
-        mlir::linalg::populateVectorizationPatterns(vectorPatterns);
+        vectorPatterns.add<GenericVectorizationPattern>(&getContext());
         if (mlir::failed(mlir::applyPatternsGreedily(getOperation(), std::move(vectorPatterns)))) {
             signalPassFailure();
         }
