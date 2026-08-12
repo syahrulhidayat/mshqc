3ab3670520db39eb466eaf7b4b82651fd896fa7e
MSHQC Automation <automation@mshqc.internal>
2026-08-13 00:02:04 +0700

    fix(compiler): integrate ownership-based buffer deallocation pass to prevent memref leaks

--- src/compiler/Passes/BufferizePass.cc
+++ src/compiler/Passes/BufferizePass.cc
@@ -1,26 +1,16 @@
 // ==============================================================================
 // Copyright (c) 2026 Muhamad Syahrul Hidayat and mshqc contributors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// ==============================================================================
+// MSHQC - Compiler Pass: Comprehensive Bufferization & Deallocation
 // ==============================================================================
 
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-
 #include "mshqc/compiler/Passes/Passes.h"
 #include "mlir/Dialect/Bufferization/IR/BufferizableOpInterface.h"
 #include "mlir/Dialect/Bufferization/IR/Bufferization.h"
 #include "mlir/Dialect/Bufferization/Transforms/OneShotAnalysis.h"
 #include "mlir/Dialect/Bufferization/Transforms/OneShotModuleBufferize.h"
+#include "mlir/Dialect/Bufferization/Transforms/Passes.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/Linalg/Transforms/BufferizableOpInterfaceImpl.h"
 #include "mlir/Dialect/Tensor/Transforms/BufferizableOpInterfaceImpl.h"
@@ -28,7 +18,8 @@
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
-
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/Passes.h"
 
 namespace mshqc {
 namespace compiler {
@@ -37,6 +28,7 @@ namespace compiler {
 #include "Passes.h.inc"
 
 namespace {
+
 struct BufferizePass : public impl::BufferizeBase<BufferizePass> {
     void getDependentDialects(mlir::DialectRegistry &registry) const override {
         registry.insert<mlir::bufferization::BufferizationDialect,
@@ -51,24 +43,41 @@ struct BufferizePass : public impl::BufferizeBase<BufferizePass> {
     }
 
     void runOnOperation() override {
+        mlir::ModuleOp module = getOperation();
+        mlir::MLIRContext *context = &getContext();
+
+        // 1. Konfigurasi One-Shot Bufferize
         mlir::bufferization::OneShotBufferizationOptions options;
         options.bufferizeFunctionBoundaries = true;
+        options.allowReturnAllocs = true; // Mengizinkan pengembalian buffer jika diperlukan sebagai output
 
-        mlir::ModuleOp module = getOperation();
-        mlir::bufferization::BufferizationState bufferizationState;
+        if (mlir::failed(mlir::bufferization::runOneShotModuleBufferize(module, options))) {
+            signalPassFailure();
+            return;
+        }
 
-        if (mlir::failed(mlir::bufferization::runOneShotModuleBufferize(
-                module, options, bufferizationState))) {
+        // 2. Terapkan Buffer Deallocation secara Global
+        mlir::PassManager pm(context);
+        
+        // Injeksi pass de-alokasi berbasis kepemilikan (Ownership-based Buffer Deallocation)
+        // Menganalisis siklus hidup memref dan menyuntikkan operasi memref.dealloc secara otomatis
+        pm.addPass(mlir::bufferization::createOwnershipBasedBufferDeallocationPass());
+        
+        // Canonicalization dan CSE membersihkan pointer dangling dan operasi memref yang berulang
+        pm.addPass(mlir::createCanonicalizerPass());
+        pm.addPass(mlir::createCSEPass());
+
+        if (mlir::failed(pm.run(module))) {
             signalPassFailure();
         }
     }
 };
 
-}
+} // end anonymous namespace
 
 std::unique_ptr<mlir::Pass> createBufferizePass() {
     return std::make_unique<BufferizePass>();
 }
 
-}
-}
+} // namespace compiler
+} // namespace mshqc
